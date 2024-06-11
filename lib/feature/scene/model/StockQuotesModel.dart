b9222f7e5993e40f810712340dd9d43d371d0034
Marcelo Luiz Pinheiro <marcelo.pinheiro@alunos.sc.senac.br>
2024-06-11 20:28:03 -0300

    finish task

---
+++ lib/feature/scene/model/StockQuotesModel.dart
@@ -0,0 +1,14 @@
+class StockQuotesModel {
+  String stock;
+  String name;
+  double close;
+  String logo;
+
+  StockQuotesModel({
+    required this.stock,
+    required this.name,
+    required this.close,
+    required this.logo
+  });
+
+}
---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+import 'feature/scene/view/StockQuotesView.dart';
+
+void main() {
+  runApp(StockQuotesView());
+}
